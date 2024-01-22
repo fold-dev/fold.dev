60d5109e6b1024e0afcb14658b5433a04dd9f68f
Jo du Plessis <jo@joduplessis.com>
2024-01-22 12:27:34 +0200

    Added correct link

--- src/pages/components/mobile.component.tsx
+++ src/pages/components/mobile.component.tsx
@@ -6,14 +6,14 @@ export default (props) => {
             className="mobile-component"
             column
             gap={10}
-            p={50}>
+            p={20}>
             <LogoSolid size="lg" />
             <Heading textAlign="center">Mobile support coming soon.</Heading>
             <Text
                 size="xl"
                 textAlign="center">
                 Please view the site on a desktop device, mobile support{' '}
-                <Link size="xl">is coming soon</Link>.
+                <Link size="xl" href="https://github.com/orgs/fold-dev/projects/8" target="_blank">is coming soon</Link>.
             </Text>
         </View>
     )
