cfaa09370d619e2baf1e9f550da0cd4ae8cb91d6
Jo du Plessis <jo@joduplessis.com>
2024-06-04 11:27:01 +0200

    Add beta text

--- src/components/core.component.tsx
+++ src/components/core.component.tsx
@@ -1485,7 +1485,7 @@ export const CoreComponent = () => {
                             Made for product teams that need to scale quickly.
                         </Heading>
                         <Text colorToken="accent-100">
-                            Introducing the Fold App Sandbox, a showcase for creating next-level product experiences with Fold.
+                            Introducing the Fold App Sandbox <sup style={{ color: 'inherit', fontWeight: 600, fontSize: 8 }}>BETA</sup>, a showcase for creating next-level product experiences with Fold.
                         </Text>
                         <Link
                             target="_blank"
