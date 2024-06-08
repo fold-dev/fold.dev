0c48bc80323167b6984221cc78489d1dd68ca204
Jo du Plessis <jo@joduplessis.com>
2024-06-08 12:00:01 +0200

    fix tag placement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import '@/styles/globals.css'
 import DocsLayout from '@/layouts/docs.layout'
 import SiteLayout from '@/layouts/site.layout'
 import BlogLayout from '@/layouts/blog.layout'
+import Head from 'next/head'
 
 export default function App(props: any) {
     const { 
@@ -15,15 +16,33 @@ export default function App(props: any) {
 
     if (route.includes('/docs')) {
         return (
-            <DocsLayout>
-                <Component {...pageProps} />
-            </DocsLayout>
+            <>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
+                    <title>Fold Documentation</title>
+                </Head>
+                <DocsLayout>
+                    <Component {...pageProps} />
+                </DocsLayout>
+            </>
         )
     } 
 
     return (
-        <SiteLayout>
-            <Component {...pageProps} />
-        </SiteLayout>
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <title>Fold</title>
+            </Head>
+            <SiteLayout>
+                <Component {...pageProps} />
+            </SiteLayout>
+        </>
     )
 }
